5bc4f77646f428e8761bc4772e9f120cb6962c83
throwmyrocketaway <jpainsgreen5@gmail.com>
2019-01-25 21:50:15 +0000

    Gender evolution graph

--- basics.py
+++ basics.py
@@ -5,10 +5,3 @@ df1 = pd.read_excel("1.xlsx")
 df2 = pd.read_excel("2.xlsx")
 df3 = pd.read_excel("3.xlsx")
 
-pos = list(range(len(df1['Survey'])))
-width = 1.0/3.0
-
-
-gender1 = utils.allgendercount(df1)
-gender2 = utils.allgendercount(df2)
-gender3 = utils.allgendercount(df3)
