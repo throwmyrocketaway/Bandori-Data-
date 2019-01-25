382e384c06ffc6ffa1d0217f5e3156a72fecf5b7
throwmyrocketaway <jpainsgreen5@gmail.com>
2019-01-25 10:13:10 +0000

    basic data
    Start clean of 3.xlsx
    
    Gender graph

--- basics.py
+++ basics.py
@@ -1,3 +1,11 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+df3 = pd.read_csv("3.xlsx")
+fig = plt.figure(figsize=(18,6))
+
+a=df3.columns.tolist()
+print a
+plt.subplot2grid((2,3),(0,0))
+df3.gender.value_counts(normalize=True).plot(kind="bar",alpha=0.5)
+plt.title("Gender")
